129d41e7ee1634095962f339fb29317c88c7a898
ant <denicad@msn.com>
2021-05-19 14:18:59 -0400

    working on landing page

--- responsive cert/landing/script.js
+++ responsive cert/landing/script.js
@@ -1,12 +1,27 @@
+const heading = document.getElementById("header");
+headerHeight = heading.getBoundingClientRect().height;
+console.log(headerHeight);
+
 const scrolling = () => {
-  const nav = document.querySelector("#nav-bar");
-  const scroll = function (e) {
-    e.preventDefault();
-    const sect = e.target.closest("a").getAttribute("href");
-    document.querySelector(`#${sect}`).scrollIntoView({
-      behavior: "smooth",
-    });
-  };
-  nav.addEventListener("click", scroll);
+	const nav = document.querySelector("#nav-bar");
+	const scroll = function (e) {
+		e.preventDefault();
+		const sect = e.target.closest("a").getAttribute("href");
+		document.querySelector(`#${sect}`).scrollIntoView({
+			behavior: "smooth",
+		});
+	};
+	nav.addEventListener("click", scroll);
 };
 scrolling();
+const options = {
+	root: null,
+	threshold: 0,
+	rootMargin: `${headerHeight}px`,
+};
+
+const headerFix = (entries, observer) => {
+	const [entry] = entries;
+	console.log(entry);
+};
+const headerObserver = new IntersectionObserver(headerFix, options);
