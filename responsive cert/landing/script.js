ec3e73ca21d095165892c397110bfcc2428dd8c7
ant <denicad@msn.com>
2021-05-19 22:07:01 -0400

    styling landing page

--- responsive cert/landing/script.js
+++ responsive cert/landing/script.js
@@ -1,27 +1,33 @@
-const heading = document.getElementById("header");
-headerHeight = heading.getBoundingClientRect().height;
-console.log(headerHeight);
-
 const scrolling = () => {
-	const nav = document.querySelector("#nav-bar");
-	const scroll = function (e) {
-		e.preventDefault();
-		const sect = e.target.closest("a").getAttribute("href");
-		document.querySelector(`#${sect}`).scrollIntoView({
-			behavior: "smooth",
-		});
-	};
-	nav.addEventListener("click", scroll);
+  const nav = document.querySelector("#nav-bar");
+  const scroll = function (e) {
+    e.preventDefault();
+    const sect = e.target.closest("a").getAttribute("href");
+    document.querySelector(`#${sect}`).scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+  nav.addEventListener("click", scroll);
 };
 scrolling();
-const options = {
-	root: null,
-	threshold: 0,
-	rootMargin: `${headerHeight}px`,
-};
+// const section = document.querySelector(`.section`);
+// const heading = document.getElementById("header");
+// const sectHeight = section.getBoundingClientRect().top;
+// const options = {
+//   root: null,
+//   threshold: 1,
+// };
+// const headerFix = (entries, observer) => {
+//   const [entry] = entries;
+//   let target = entry.target;
+//   console.log(entry);
 
-const headerFix = (entries, observer) => {
-	const [entry] = entries;
-	console.log(entry);
-};
-const headerObserver = new IntersectionObserver(headerFix, options);
+//   if (entry.isIntersecting) {
+//     console.log(entry);
+//     heading.classList.add("sticky");
+//     return;
+//   }
+// };
+// const headerObserver = new IntersectionObserver(headerFix, options);
+
+// headerObserver.observe(section);
