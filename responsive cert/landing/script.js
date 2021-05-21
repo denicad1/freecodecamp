58ccc48fda58eeabe0e066fd26e8b69139d4d02e
ant <denicad@msn.com>
2021-05-20 20:24:36 -0400

    styling landing page

--- responsive cert/landing/script.js
+++ responsive cert/landing/script.js
@@ -1,18 +1,22 @@
 const scrolling = () => {
-  const nav = document.querySelector("#nav-bar");
+  const nav = document.getElementById("header");
   const scroll = function (e) {
     e.preventDefault();
     const sect = e.target.closest("a").getAttribute("href");
-    document.querySelector(`#${sect}`).scrollIntoView({
-      behavior: "smooth",
-    });
+    const navHeight = nav.getBoundingClientRect().height;
+    const correctSect =
+      document.getElementById(`${sect}`).getBoundingClientRect().top +
+      window.pageYOffset -
+      (navHeight + 20);
+    window.scrollTo({ top: correctSect, behavior: "smooth" });
+    // document.querySelector(`#${sect}`).scrollIntoView({
+    //   behavior: "smooth",
+    // });
   };
   nav.addEventListener("click", scroll);
 };
 scrolling();
 // const section = document.querySelector(`.section`);
-const heading = document.getElementById("header");
-const headerHeight = heading.getBoundingClientRect().height;
 // const options = {
 //   root: null,
 //   threshold: 0,
@@ -27,22 +31,25 @@ const headerHeight = heading.getBoundingClientRect().height;
 //     return;
 //   }
 // };
-const headerObserver = new IntersectionObserver(headerFix, options);
+// const headerObserver = new IntersectionObserver(headerFix, options);
 // headerObserver.observe(section);
-const sections = document.querySelectorAll(".section h1");
-const opts = {
-  root: null,
-  threshold: 0,
-};
-const slideUp = (entries, observer) => {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
+const titleRaise = () => {
+  const sections = document.querySelectorAll(".section h1");
+  const opts = {
+    root: null,
+    threshold: 0,
+  };
+  const slideUp = (entries, observer) => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
 
-  entry.target.classList.add("animate__animated", "animate__bounceInUp");
-  observer.unobserve(entry.target);
-};
-const sectObserver = new IntersectionObserver(slideUp, opts);
+    entry.target.classList.add("animate__animated", "animate__bounceInUp");
+    observer.unobserve(entry.target);
+  };
+  const sectObserver = new IntersectionObserver(slideUp, opts);
 
-sections.forEach((section) => {
-  sectObserver.observe(section);
-});
+  sections.forEach((section) => {
+    sectObserver.observe(section);
+  });
+};
+titleRaise();
